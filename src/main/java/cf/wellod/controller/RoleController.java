cd61ea1b15f26d9d3fde7d93767d3b59777a5940
kervia <3032964558@qq.com>
2020-09-28 10:52:17 +0800

    fix

--- src/main/java/cf/wellod/controller/RoleController.java
+++ src/main/java/cf/wellod/controller/RoleController.java
@@ -6,18 +6,15 @@ import cf.wellod.utils.DateUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 @Controller
 @ResponseBody
-@RequestMapping("/api/admin/role")
 public class RoleController {
     @Autowired
     RoleMapper roleMapper;
@@ -39,6 +36,110 @@ public class RoleController {
                 retJson.put("code",-1);
                 retJson.put("msg", "failed");
             }
+        }
+        return retJson;
+    }
+
+    //更新role表格数据
+    @GetMapping("/admin/roles")
+    public HashMap<String,Object> getRoles(@RequestParam("page")Integer page, @RequestParam("limit") Integer limit){
+        HashMap<String,Object> retJson = new HashMap<>();
+        Integer count = roleMapper.getRolesCount();
+        if(count >= 0 && limit > 0 && page > 0)
+        {
+            int pageNum = (int)Math.ceil(count.floatValue()/limit);
+            if(pageNum == 0) pageNum++;
+            if(page > pageNum) page = pageNum;
+            int start = (page-1)*limit+1;
+            System.out.println("start "+start);
+            retJson.put("code", 0);
+            retJson.put("msg", "success");
+            retJson.put("count", count);
+            List<Role> list = roleMapper.getEmpsByRange(start,limit);
+            retJson.put("data", list);
+            System.out.println(list);
+        }else{
+            retJson.put("code", 0);
+            retJson.put("msg", "failed");
+            retJson.put("count", 0);
+            retJson.put("data", new ArrayList<Role>());
+        }
+
+        return retJson;
+    }
+
+    //批量删除Roles
+    @DeleteMapping("/admin/role")
+    public Object deleteRoles(@RequestBody List<Integer> list){
+        HashMap<String,Object> retJson = new HashMap<String,Object>();
+        try{
+            if(list.size() != 0)
+            {
+                roleMapper.deleteRoles(list);
+                retJson.put("code", 0);
+                retJson.put("msg", "success");
+            }else{
+                retJson.put("code", -1);
+                retJson.put("msg", "操作数量不能为0");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+            retJson.put("code", -1);
+            retJson.put("msg", "failed");
+        }
+        return retJson;
+
+    }
+
+    //删除某个Role
+    @DeleteMapping("/admin/role/{id}")
+    public HashMap<String,Object> deleteRoleById(@PathVariable(value = "id")Integer id){
+        HashMap<String,Object> retJson = new HashMap<String,Object>();
+        try{
+            roleMapper.deleteRoleById(id);
+            retJson.put("code", 0);
+            retJson.put("msg", "success");
+        }catch (Exception e){
+            retJson.put("code", -1);
+            retJson.put("msg", "failed");
+        }
+        return retJson;
+    }
+
+    //插入一个Role
+    @PostMapping("/admin/role/")
+    public HashMap<String,Object> insertRole(@RequestBody Role role){
+        HashMap<String,Object> retJson = new HashMap<String,Object>();
+        if(role.getName() != null && role.getInfo() != null && role.getId() == null)
+        {
+            retJson.put("code", 0);
+            retJson.put("msg", "操作成功");
+            try{
+                roleMapper.insertRole(role);
+            }catch (Exception e){
+                System.out.println(e);
+                retJson.put("code", -1);
+                retJson.put("msg", "操作失败");
+            }
+        }else{
+            retJson.put("code", -1);
+            retJson.put("msg", "操作失败");
+        }
+        return retJson;
+    }
+
+    //更新某个Role
+    @PutMapping("/admin/role/")
+    public Object updateRole(@RequestBody Role role){
+        HashMap<String,Object> retJson = new HashMap<String,Object>();
+        try{
+            roleMapper.updateRole(role);
+            retJson.put("code", 0);
+            retJson.put("msg", "success");
+        }catch (Exception e){
+            System.out.println(e);
+            retJson.put("code", -1);
+            retJson.put("msg", "failed");
         }
         return retJson;
     }
